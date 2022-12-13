1dd798bc2eb630a26e7012a0c3f9cce7929ca69f
Dan Schreiber <dmschreiber@gmail.com>
2022-12-13 05:59:52 -0500

    clean on day 13

--- day13.py
+++ day13.py
@@ -82,14 +82,3 @@ if __name__ == '__main__':
     input_file = "./data/" + d + "_input.txt"
     part1(input_file)
     part2(input_file)
-
-    # lst = [1, 4, 4, 4, 2, 5, 6, 6, 7, 8, 9, 10]
-    # print(scrib.find_most_frequent(lst))
-    # print(scrib.find_occurances(lst)[4])
-    # print(scrib.find_even(lst))
-    # print(scrib.capitalize_words(["python", "javaScript", "c++"]))
-    # print(scrib.reverse_list(lst))
-
-    # grid = {(0,0): 1, (1,0): 1, (2,0):1 }
-    # grid = scrib.life(grid,5)
-    # print(sum([grid.get(d) if grid.get(d) is not None else 0 for d in grid.keys()]))
